7e22a718d247fd73798d9547427c937107da8453
Harshal Gulhane <98465047+HardshellG@users.noreply.github.com>
2024-03-09 01:46:10 +0530

    Update home.py

--- home.py
+++ home.py
@@ -1,6 +1,8 @@
 
 import streamlit as st
-
+import time
+import random
+import numpy as np
 import pandas as pd
 import random
 def app():
@@ -8,8 +10,6 @@ def app():
      st.title("Home") 
      
 st.sidebar.success("Mini Project")                                  
-
-
 
 st.write("Welcome to the Exam Timeline website, your one-stop destination for comprehensive and up-to-date information on examination schedules, deadlines, and important dates. Whether you're a student, educator, or professional seeking to stay organized, our platform is designed to help you navigate the world of exams with ease and confidence.")
 
