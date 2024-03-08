a566f2f6438304113ac41a0327b66296622355d7
Harshal Gulhane <98465047+HardshellG@users.noreply.github.com>
2024-03-09 02:03:19 +0530

    Update home.py

--- home.py
+++ home.py
@@ -7,7 +7,7 @@ import pandas as pd
 import random
 def app():
      st.title("Home")                              
-st.write("Welcome to the Exam Timeline website, your one-stop destination for comprehensive and up-to-date information on examination schedules, deadlines, and important dates. Whether you're a student, educator, or professional seeking to stay organized, our platform is designed to help you navigate the world of exams with ease and confidence.")
+     st.write("Welcome to the Exam Timeline website, your one-stop destination for comprehensive and up-to-date information on examination schedules, deadlines, and important dates. Whether you're a student, educator, or professional seeking to stay organized, our platform is designed to help you navigate the world of exams with ease and confidence.")
 
     
 
