b004607481a58917abd0834c5d043602f7fee0c5
Harshal Gulhane <98465047+HardshellG@users.noreply.github.com>
2024-03-09 02:14:47 +0530

    Update homepage.py

--- homepage.py
+++ homepage.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from streamlit_option_menu import option_menu
 
-import home,Engineering 
+import home,Engineering,predictor
 st.set_page_config(
         page_title="ExamTimeLine",
 )
