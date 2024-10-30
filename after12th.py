d07d670c40ca077ae5c60cb0803e3cdfe0c11029
Harshal Gulhane <98465047+HardshellG@users.noreply.github.com>
2024-10-31 00:50:38 +0530

    Update after12th.py

--- after12th.py
+++ after12th.py
@@ -2,8 +2,16 @@ import streamlit as st
 import pandas as pd
 
 def app():
-df=pd.read_csv("after12th.csv")
-   st.write(df)
+data = {
+    'Exam name': ['JEE Main','MHTCET'],
+    'Website': ['https://jeemain.nta.nic.in/','https://cetcell.mahacet.org/CAP_landing_page_2023/']
+}
+
+
+df = pd.DataFrame(data)
+
+st.dataframe(df, column_config={"Website": st.column_config.LinkColumn()})
+
 
  
 
