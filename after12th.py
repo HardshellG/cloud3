446ae1f69d3dda800744eda866db531ea19fb200
Harshal Gulhane <98465047+HardshellG@users.noreply.github.com>
2024-10-31 00:56:43 +0530

    Update after12th.py

--- after12th.py
+++ after12th.py
@@ -2,15 +2,15 @@ import streamlit as st
 import pandas as pd
 
 def app():
-data = {
-    'Exam name': ['JEE Main','MHTCET'],
-    'Website': ['https://jeemain.nta.nic.in/','https://cetcell.mahacet.org/CAP_landing_page_2023/']
-}
-
-
-df = pd.DataFrame(data)
-
-    st.dataframe(df, column_config={"Website": st.column_config.LinkColumn()})
+    data = {
+        'Exam name': ['JEE Main','MHTCET'],
+        'Website': ['https://jeemain.nta.nic.in/','https://cetcell.mahacet.org/CAP_landing_page_2023/']
+    }
+    
+    
+    df = pd.DataFrame(data)
+    
+        st.dataframe(df, column_config={"Website": st.column_config.LinkColumn()})
 
 
  
